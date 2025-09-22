8b1f5ff54007149613a2e69edf49a56b7f88da14
CGarcia10T <garcia.cu@obmontessori.edu.ph>
2025-09-22 13:05:10 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from pyscript import display
+
+# string data type
+resturant_name = "The Cornerstore"
+owner_name = "Calvin Garcia"
+
+# integer data type
+year_established = 2009
+
+# float data type
+popular_item_price = 15.99
+tax_rate = 0.085
+
+# boolean data type
+has_delivery = True
+
+# list data type
+product_names = ["Four Cheese Pizza", "Pepperoni Pizza", "Margherita Pizza", "Spinach Pizza", "Hawaiian Pizza", "Meat Lover's Pizza"]
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+common_allergens = ["Dairy", "Soy", "Eggs", "Wheat", "Tomato"]
+
+# dictionary data type
+business_hours = {"Monday": "7:00AM-9:00PM", "Tuesday": "7:00AM-9:00PM", "Wednesday": "7:00AM-9:00PM", "Thursday": "7:00AM-9:00PM", "Friday": "7:00AM-7:00PM", "Saturday": "7:00AM-7:00PM", "Sunday": "7:00AM-7:00PM"}
+menu_prices = {"Four Cheese Pizza": 499.99, "Pepperoni Pizza": 399.99, "Margherita Pizza": 399.99, "Spinach Pizza": 399.99, "Hawaiian Pizza": 350.99, "Meat Lover's Pizza": 499.99,}
